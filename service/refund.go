fa388a567e6df12fdc1aab2ea6fa2f24679a67a1
ragga <965061947@qq.com>
2026-05-15 10:51:51 +0800

    测试退款

--- service/refund.go
+++ service/refund.go
@@ -112,8 +112,8 @@ func refundWeChatPay(tradeNo string, refundAmountCents int64, totalAmountCents i
 		OutRefundNo: core.String(outRefundNo),
 		Reason:      core.String("用户申请退款"),
 		Amount: &refunddomestic.AmountReq{
-			Refund:   core.Int64(refundAmountCents),
-			Total:    core.Int64(totalAmountCents),
+			Refund:   core.Int64(2),
+			Total:    core.Int64(2),
 			Currency: core.String("CNY"),
 		},
 	})
